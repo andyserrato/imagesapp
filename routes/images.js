e778e52a17a59baa2ee0e08cf3c9ac424dc19f34
andyserrato <andyserrato@hotmail.com>
2018-05-31 00:18:57 +0200

    some changes

--- routes/images.js
+++ routes/images.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const router = express.Router();
-
+const path = require('path');
+const fs = require('fs');
+const IMAGES_ROUTE = 'public/images/shoes';
 // GET
 // images/ serve a list of all images
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('hola');
-  });
-  
-  module.exports = router;
+router.get('/', function (req, res, next) {
+  const shoesPath = path.join(__dirname, 'public/images/shoes');
+  console.log(shoesPath);
+  console.log(fs.statSync(IMAGES_ROUTE).isDirectory());
+  fs.readdir(shoesPath, (err, files) => {
+    if (files)
+      files.forEach(file => {
+        console.log(file);
+      });
+    else
+      console.log(files)
+  })
+
+  res.send('hola');
+});
+
+module.exports = router;
 
 // images/random serve an image randomly
 // images/{name}
 
 // POST
 //images/ upload an image be carefull with the size limit and everything
-
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+
+  
+
   res.send('hola with a resource');
 });
 
